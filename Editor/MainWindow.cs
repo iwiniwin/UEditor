d5d6d0ad7c0ed597b07ff71f2d77ca4d4d31cb09
iwiniwin <iwiniwin@foxmail.com>
2020-06-16 22:19:00 +0800

    调整目录结构

--- Editor/MainWindow.cs
+++ Editor/MainWindow.cs
@@ -9,19 +9,19 @@ public class MainWindow : EditorWindow
         // 描述，宏
         {"清除无用资源（Assets/Extend/Clear）", "UEditor_A0"},
         {"查找重复资源（Assets/Extend/Find Duplicate Resources）", "UEditor_A1"},
+        {"根据自定义模板新建脚本并打开（Assets/Create/C# Stand Script）", "UEditor_A2"},
 
         {"打开编辑器所在目录（File/Open Editor Folder）", "UEditor_F0"},
         {"打开工程所在目录（File/Open Project Folder）", "UEditor_F1"},
 
         {"程序集重新加载时是否弹出提示", "UEditor_C0"},
-
     };
 
     static bool[] options = new bool[Features.GetLength(0)];
 
     static HashSet<string> macroSet = null;
 
-    [MenuItem("Window/Editor Window")]
+    [MenuItem("Window/UEditor Window")]
     static void InitMainWindow(){
         string macroStr = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone);
         string[] macros = macroStr.Split(';');
@@ -32,17 +32,17 @@ public class MainWindow : EditorWindow
             else
                 options[i] = false;
         }
-        EditorWindow.GetWindow(typeof(MainWindow), false, "Editor Window").minSize = new Vector2(500, 200);
+        EditorWindow.GetWindow(typeof(MainWindow), false, "UEditor Window").minSize = new Vector2(500, 200);
     }
 
     Vector2 scrollPos;
-    int bottomAreaHeight = 25;
+    int bottomAreaHeight = 24;
     int buttonGap = 5;
 
     void OnGUI(){
 
         GUIStyle style = new GUIStyle();
-        style.fontSize = 14;
+        style.fontSize = 15;
         style.padding = new RectOffset(5, 5, 5, 5);
         GUILayout.Label("启用/禁用以下编辑器扩展", style);
 
@@ -54,8 +54,18 @@ public class MainWindow : EditorWindow
 
         EditorGUILayout.EndScrollView();
 
-        GUILayout.BeginArea( new Rect(buttonGap, position.height - bottomAreaHeight, position.width - buttonGap * 2, position.height));
 
+        GUIStyle areaStyle = new GUIStyle();
+        Color color;
+        ColorUtility.TryParseHtmlString("#a2a2a2", out color);
+        Texture2D texture = new Texture2D(128, 128);
+        Color[] colors = texture.GetPixels();
+        for(int i = 0; i < colors.Length; i ++)
+            colors[i] = color;
+        texture.SetPixels(colors);
+        texture.Apply();
+        areaStyle.normal.background = texture;
+        GUILayout.BeginArea( new Rect(0, position.height - bottomAreaHeight, position.width, position.height), areaStyle);
 
         EditorGUILayout.BeginHorizontal();
         GUILayout.FlexibleSpace();
@@ -79,7 +89,7 @@ public class MainWindow : EditorWindow
         Color color;
         ColorUtility.TryParseHtmlString("#434343", out color);
         style.normal.textColor = color;
-        style.fontSize = 12;
+        style.fontSize = 13;
         // left --
         style.padding = new RectOffset(10, 0, 0, 0);
         GUILayout.Label(text, style);
