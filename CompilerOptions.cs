1ae284156653bf74ea8c19c6f19ead18094c2e19
iwiniwin <iwiniwin@foxmail.com>
2020-06-16 11:00:48 +0800

    添加编辑器正在编译提示

---
+++ CompilerOptions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class CompilerOptions 
+{
+    static CompilerOptions(){
+        // EditorApplication.update += OnEditorUpdate;
+        AssemblyReloadEvents.beforeAssemblyReload += OnBeforeAssemblyReload;
+        AssemblyReloadEvents.afterAssemblyReload += OnAfterAssemblyReload;
+    }
+
+    static void OnEditorUpdate(){
+        if(EditorApplication.isCompiling) {
+            EditorUtility.DisplayProgressBar("Script Compiling", "please wait for a moment ......", 1.0f);
+        }
+        if(EditorUtility.scriptCompilationFailed){
+            EditorUtility.ClearProgressBar();
+        }
+    }
+
+    // [UnityEditor.Callbacks.DidReloadScripts]
+    // static void OnReloadScripts(){
+    //     EditorUtility.ClearProgressBar();
+    // }
+
+    static void OnBeforeAssemblyReload() {
+        EditorUtility.DisplayProgressBar("Script Compiling", "please wait for a moment ......", 1.0f);
+    }
+
+    static void OnAfterAssemblyReload() {
+        EditorUtility.ClearProgressBar();
+    }
+}
