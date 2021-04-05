9175dfb1b9a5503d4d42f89897a43ba12d117ade
iwiniwin <iwiniwin@foxmail.com>
2021-04-05 21:12:33 +0800

    添加扩展复制游戏对象的相对路径

--- Editor/MainWindow.cs
+++ Editor/MainWindow.cs
@@ -15,6 +15,8 @@ public class MainWindow : EditorWindow
         {"打开编辑器所在目录（File/Open Editor Folder）", "UEditor_F0", ""},
         {"打开工程所在目录（File/Open Project Folder）", "UEditor_F1", ""},
 
+        {"复制场景中游戏对象的相对路径（GameObject/Copy Relative Path）", "UEditor_G0", ""},
+
         {"清除无用资源（Assets/Extend/Clear）", "UEditor_A0", ""},
         {"查找重复资源（Assets/Extend/Find Duplicate Resources）", "UEditor_A1", ""},
         {"根据自定义模板新建脚本并打开（Assets/Create/C# Stand Script）", "UEditor_A2", ""},
---
+++ Editor/Menus/GameObjectMenuExtend.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UEditor
+{
+    public class GameObjectMenuExtend
+    {
+#if UEditor_G0
+    /// <summary>
+    /// 复制场景中游戏对象的相对路径
+    /// </summary>
+    [MenuItem("GameObject/Copy Relative Path", false, 20)]
+    static void CopyRelativePath(){
+        UnityEngine.Object obj = Selection.activeObject;
+        if(obj == null) return;
+        string result = AssetDatabase.GetAssetPath(obj);
+        if(string.IsNullOrEmpty(result)) {
+            Transform selectChild = Selection.activeTransform;
+            if(selectChild != null) {
+                result = selectChild.name;
+                while(selectChild.parent != null) {
+                    selectChild = selectChild.parent;
+                    result = string.Format("{0}/{1}", selectChild.name, result);
+                }
+            }
+        }
+        // 复制到剪贴板
+        TextEditor editor = new TextEditor();
+        editor.content = new GUIContent(result);
+        editor.OnFocus();
+        editor.Copy();
+    }
+#endif
+    }
+}
+
+
